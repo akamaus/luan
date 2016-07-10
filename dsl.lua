92206e934719f2ad630b9d147624e6d53eb39e3d
Dmitry Vyal <akamaus@gmail.com>
2016-07-10 12:06:32 +0300

    graph evaluation and drawing

--- dsl.lua
+++ dsl.lua
@@ -1,9 +1,19 @@
-
-
+-- For use of the global environment from this scope.
+local strict = require "std.strict"
+local _ENV = strict (_G)
 
 local Plus = '+'
 local Mul = '*'
 local Lt = '<'
+
+local OpTable = {
+  [Plus] = { f = function(a,b) return a+b end,
+             cost = 1
+           },
+  [Mul] = { f = function(a,b) return a*b end,
+            cost = 10
+          }
+}
 
 local BinOp
 
@@ -24,67 +34,74 @@ local function Var(v)
   o.var = v
   o.type = 'var'
   setmetatable(o, node_mt)
+  return o
 end
 
-local function BinOp(op, a,b)
+local function Num(n)
   local o = {}
-  o.op = op
-  o.arg1 = a
-  o.arg2 = b
+  o.num = n
+  o.type = 'num'
+  setmetatable(o, node_mt)
+  return o
+end
+
+local function wrapNode(x)
+  if type(x) == 'table' then
+    assert(x.type)
+    return x
+  elseif type(x) == 'number' then
+    return Num(x)
+  else error('unknown type passed to WrapNode: ' .. type(x))
+  end
+end
+
+
+BinOp = function(op, a,b)
+  local o = {}
+  o.bin_op = op
+  o.arg1 = wrapNode(a)
+  o.arg2 = wrapNode(b)
   o.type = 'bin_op'
   setmetatable(o, node_mt)
+  return o
 end
 
 -- walking graph
 
 local function walk_graph(g, callback)
   local unvisited = { g }
-  local visited = {}
+  local known = {}
 
   local function enqueue_if_new(n)
-    if type(n) == 'table' then
-      if visited[n] then return end
-      table.insert(unvisited, n)
-      visited[n] = true
-    end
+    assert(type(n) == 'table')
+    if known[n] then return end
+    table.insert(unvisited, n)
+    known[n] = true
   end
 
   repeat
-    n = table.remove(unvisited)
-    if type(n) == 'number' then callback(n)
-    elseif type(n) == 'table' then
-      if n.type == 'bin_op' then
-        enqueue_if_new(n.arg1)
-        enqueue_if_new(n.arg2)
-      end
-      callback(n)
-    else assert('unknown node type' .. type(n))
+    local n = table.remove(unvisited)
+    assert(type(n) == 'table' and n.type)
+    if n.type == 'bin_op' then
+      enqueue_if_new(n.arg1)
+      enqueue_if_new(n.arg2)
+    end
+    callback(n)
   until #unvisited == 0
 end
 
 -- rendering
 
-
 function render_graph(graph, file)
    local f = io.open(file, "wb")
-   f:write("expr {\n")
+   f:write("digraph expr {\n")
 
    local node_indices = {}
    local last_idx = 0
 
-   local function render_num(n)
-     assert(type(n) == 'number')
-
-     local idx = last_idx
-     last_idx = last_idx + 1
-     f:write(string.format('  v%d [label="%d"]\n', idx, n))
-     return idx
-   end
-
    local function reify_node(n)
-     if type(n) ~= 'table' then
-       return nil
-     end
+     assert(type(n) == 'table')
+
      if node_indices[n] then
        return node_indices[n]
      else
@@ -94,43 +111,57 @@ function render_graph(graph, file)
      end
    end
 
-   walk_graph(graph, reify_node)
-
-
    local function render_node(n)
-     if type(n) == 'table' then
-       if n.type == 'var' then
-         f:write(string.format('  v%d [label="%s"]', reify_node[n], n.var))
-       elseif n.type == '
-   
-   local function render_branch(b)
-      f:write(string.format('  v%d [label="%s\np=%.2f\ns=%d"]\n', reify_node(b), b.criterion:Stringify(), b.prec, b.size))
-      f:write(string.format('  v%d -> v%d [color=red];\n', reify_node(b), reify_node(b.neg)))
-      f:write(string.format('  v%d -> v%d [color=blue];\n\n', reify_node(b), reify_node(b.pos)))
+     assert(n.type)
+     if n.type == 'num' then
+       f:write(string.format('  v%d [label="%d" color="green"];\n', reify_node(n), n.num))
+     elseif n.type == 'var' then
+       f:write(string.format('  v%d [label="%s" color="blue"];\n', reify_node(n), n.var))
+     elseif n.type == 'bin_op' then
+       f:write(string.format('  v%d [label="%s"] [shape="box"];\n', reify_node(n), n.bin_op))
+     else error("unknown type " .. n.type)
+     end
    end
 
-   local function render_leaf(l)
-      local color
-      if l.answer then
-         color = "green"
-      else
-         color = "brown"
-      end
-      f:write(string.format('  v%d [label="p=%.2f\ns=%d"] [shape=box] [color=%s]\n', reify_node(l), l.prec, l.size, color))
+   local function render_deps(n)
+     if n.type ~= 'bin_op' then return end
+     f:write(string.format('  v%d -> v%d;\n', reify_node(n), reify_node(n.arg1)))
+     f:write(string.format('  v%d -> v%d;\n', reify_node(n), reify_node(n.arg2)))
    end
 
-   walk_tree(tree, render_branch, render_leaf)
+   walk_graph(graph, render_node)
+   walk_graph(graph, render_deps)
 
    f:write("}")
    f:close()
 end
 
+local function eval_node(node, env)
+  local val, cost
+  if node.type == 'num' then
+    val,cost = node.num, 0
+  elseif node.type == 'var' then
+    val = assert(env[node.var], 'unknown variable ' .. node.var)
+    cost = 0
+  elseif node.type == 'bin_op' then
+    local op = assert(OpTable[node.bin_op], 'unknown op')
+    local arg1,cost1 = eval_node(node.arg1, env)
+    local arg2,cost2 = eval_node(node.arg2, env)
 
+    val = op.f(arg1, arg2)
+    cost = op.cost + cost1 + cost2
+  else
+    error('cant evaluate ' .. node.type)
+  end
 
+  return val, cost
+end
 --testing
 
-a = Var "a"
-b = Var "b"
+a = Var 'a'
+b = Var 'b'
 
-res = 3 * a + b + 5 * a + 10 * b
+res = 3 * a + b + 5 * a -- + 10 * b
 
+render_graph(res, 'tst.dot')
+print(eval_node(res, { a = 10, b = 20 }))
