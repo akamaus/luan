b6e89e3b5a7ac2802d0f3f04121d5b195ddb3c74
Dmitry Vyal <akamaus@gmail.com>
2016-07-10 19:06:11 +0300

    assoc_l rule and test

--- transform.lua
+++ transform.lua
@@ -16,6 +16,18 @@ local TransformRules = {
     apply = function(n)
       n.arg1, n.arg2 = n.arg2, n.arg1
     end
+  },
+  assoc_l = {
+    match = function(n)
+      if n.type == 'bin_op' and n.bin_op == n.arg1.bin_op then
+        return { n_l = n.arg1, n_r = n}
+      end
+    end,
+    apply = function(s)
+      local l = assert(s.n_l)
+      local r = assert(s.n_r)
+      l.arg1, l.arg2, r.arg1, r.arg2 = l.arg2, r.arg2, l.arg1, l
+    end
   }
 }
 
@@ -24,8 +36,9 @@ function M.find_transform_sites(graph)
 
   local function detect_possibility(n)
     for name, rule in pairs(TransformRules) do
-      if rule.match(n) then
-        table.insert(sites, { rule = name, place = n})
+      local m = rule.match(n)
+      if m then
+        table.insert(sites, { rule = name, place = m})
       end
     end
   end
